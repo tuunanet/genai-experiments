658911afc22d2b7d46ef5b0ed0e558df578c2cea
Tuomo Tuunanen <47913151+tuunanet@users.noreply.github.com>
2025-10-24 23:24:16 +0300

    Improved examples.

--- notebooks/openai_api_basics.py
+++ notebooks/openai_api_basics.py
@@ -9,30 +9,18 @@ def _():
     import marimo as mo
     from dotenv import load_dotenv, find_dotenv
     from pathlib import Path
+    import asyncio
     from openai import OpenAI
-    return OpenAI, Path, find_dotenv, load_dotenv, mo
-
-
-@app.cell(hide_code=True)
-def _(mo):
-    mo.md(r"""# OpenAI client instance""")
-    return
+    from openai import AsyncOpenAI
+    return AsyncOpenAI, OpenAI, Path, asyncio, find_dotenv, load_dotenv, mo
 
 
 @app.cell
-def _(OpenAI, Path, find_dotenv, load_dotenv):
+def _(Path, find_dotenv, load_dotenv, mo):
     # Grab the API key from detected .env file and create a client. By default the client checks for OPENAI_API_KEY environment variable.
     path_dotenv_file = Path(find_dotenv())
     load_dotenv(path_dotenv_file, override=True)
-    client = OpenAI()
-    return (client,)
-
-
-@app.cell
-def _(client):
-    # Let's see what models we've got
-    for model in client.models.list():
-        print(model)
+    mo.stop()
     return
 
 
@@ -40,28 +28,66 @@ def _(client):
 def _(mo):
     mo.md(
         r"""
-    # OpenAI Completion vs. Responses API
+    # OpenAI Responses API
 
-    I noticed many online courses have been made before responses API has become available, but fortunately OpenAI provides a [migration guide](https://platform.openai.com/docs/guides/migrate-to-responses) we can refer to when still seeing code using the old Chat completions API.
+    I noticed a useful [migration guide](https://platform.openai.com/docs/guides/migrate-to-responses) for those more familiar with Chat completions API. Let's try do some basic streaming responses with it.
     """
     )
     return
 
 
 @app.cell
-def _(client):
-    # Invoke an output response with the instance of the initialized responses client.
-    response = client.responses.create(
-        model="gpt-5-nano",
-        input="Write a short bedtime story about a unicorn."
-    )
+def _(OpenAI):
+    client = OpenAI()
 
-    print(response.output_text)
+    # If you're using pay-as-you-go tier like me, cold start probably takes 15-20 secs before getting any streamed output
+    with client.responses.create(
+        model="gpt-5-nano",
+        input=[
+            {
+                "role": "user",
+                "content": "Write a riddle consisting of ten sentences.",
+            },
+        ],
+        stream=True,
+    ) as stream:
+        for event in stream:
+            if event.type == "response.output_text.delta":
+                # Note: always use flush=True so that text appears in real-time
+                print(event.delta, end="", flush=True)
+            elif event.type == "response.completed":
+                print("\n\n--- Completed streaming")
     return
 
 
 @app.cell
-def _():
+def _(mo):
+    mo.md(
+        r"""
+    # Async chat responses?
+
+    It would be nice to try out async method for OpenAI chat responses, but I do not yet know if I can do that within a Marimo notebook. I will write a potential example here for later reference (or to copy paste and try out in the terminal)
+    """
+    )
+    return
+
+
+@app.cell
+def _(AsyncOpenAI, asyncio):
+    client_async = AsyncOpenAI()
+
+    async def main():
+        async with client_async.responses.stream(
+            model="gpt-5-nano",
+            input=[{"role": "user", "content": "Write a riddle consisting of ten sentences."}],
+        ) as stream:
+            async for event in stream:
+                if event.type == "response.output_text.delta":
+                    print(event.delta, end="", flush=True)
+                elif event.type == "response.completed":
+                    print("\n\n--- Completed streaming")
+
+    asyncio.run(main())
     return
 
 
